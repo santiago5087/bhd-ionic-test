50687a20c8cab4dd5763692474d6eb0779f65190
Edhy Santiago Marín <santiago.marin@pragma.com.co>
2021-08-15 23:48:12 -0500

    Styling pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,14 @@ const routes: Routes = [
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
+    path: 'contacto',
+    loadChildren: () => import('./pages/contacto/contacto.module').then( m => m.ContactoPageModule)
+  },
+  {
+    path: 'sucursales',
+    loadChildren: () => import('./pages/sucursales/sucursales.module').then( m => m.SucursalesPageModule)
+  },
+  {
     path: 'tabs',
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,19 +15,33 @@ export class AppComponent {
   pages = [
     {
       title: 'Mis productos',
-      url: '/tabs/mis-productos'
+      url: '/tabs/mis-productos',
+      icon: 'assets/images/icon_my_products_green.svg'
     },
     {
       title: 'Transacciones',
-      url: '/tabs/transacciones'
+      url: '/tabs/transacciones',
+      icon: 'assets/images/icon_transactions_green.svg'
     },
     {
       title: 'Ofertas',
-      url: '/tabs/ofertas'
+      url: '/tabs/ofertas',
+      icon: 'assets/images/icon_offers_green.svg'
     },
     {
       title: 'Configuración',
-      url: '/tabs/configuracion'
+      url: '/tabs/configuracion',
+      icon: 'assets/images/icon_config_green.svg'
+    },
+    {
+      title: 'Contacto',
+      url: '/contacto',
+      icon: 'assets/images/icon_contact.svg'
+    },
+    {
+      title: 'Sucursales',
+      url: '/sucursales',
+      icon: 'assets/images/icon_branches.svg'
     }
   ];
 
